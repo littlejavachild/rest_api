eecaf9d1e763f55b35cb0f307da9508546bcb9c3
Fasih Khatib <fasih.khatib@raweng.com>
2016-11-04 20:07:08 +0530

    added pagination

--- v1/search_product.js
+++ v1/search_product.js
@@ -9,6 +9,7 @@ module.exports = function(request,response,db){
     return co(function*(){
         let id = request.url.split("/").pop();
         let bearer = request.headers["authorization"].split(" ").pop();
+        let numberRegex = /^\d+(\.\d{1,2})?$/;
 
         if( !(yield auth.isValidToken(bearer)) ){
             let _403 = require("./_403");
@@ -20,10 +21,58 @@ module.exports = function(request,response,db){
             let parsed = url.parse(request.url,true);
 
             // remember to parse the strings to numbers
-            
+            // remove them if the value cannot be parsed to number
+            if( parsed.query.price  ){
+                if(!numberRegex.test(parsed.query.price)){
+                    delete parsed.query.price;
+                }else{
+                    parsed.query.price = parseFloat( parsed.query.price );
+                }
+            }
 
-            yield db.search(parsed.query);
-            response.end();
+            if( parsed.query.quantity  ){
+                if(!numberRegex.test(parsed.query.quantity)){
+                    delete parsed.query.quantity;
+                }else{
+                    parsed.query.quantity = parseFloat( parsed.query.quantity );
+                }
+            }
+
+            if( parsed.query.status  ){
+                if(!numberRegex.test(parsed.query.status)){
+                    delete parsed.query.status;
+                }else{
+                    parsed.query.status = parseFloat( parsed.query.status );
+                }
+            }
+
+            if( parsed.query.perPage  ){
+                if(!numberRegex.test(parsed.query.perPage)){
+                    delete parsed.query.perPage;
+                }else{
+                    parsed.query.perPage = parseFloat( parsed.query.perPage );
+                }
+            }
+
+            if( parsed.query.page  ){
+                if(!numberRegex.test(parsed.query.page)){
+                    delete parsed.query.page;
+                }else{
+                    parsed.query.page = parseFloat( parsed.query.page );
+                }
+            }
+
+            let result = yield search(parsed.query,db);
+
+            response.writeHead(200,{
+                "Content-Type" : "application/json"
+            });
+
+            response.end(JSON.stringify({
+                success : true,
+                code: 200,
+                result : result
+            }));
         }
     }).catch((e)=>{
         console.error( e );
@@ -33,6 +82,8 @@ module.exports = function(request,response,db){
 
 //--------------------------------------------------------------------
 
-
+function* search(options,db){
+    return yield db.search(options);
+}
 
 //--------------------------------------------------------------------
