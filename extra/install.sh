58c106bf4e007913e2fa33bdc16d31fff3161053
Fasih Khatib <fasih.khatib@raweng.com>
2016-11-05 17:01:59 +0530

    modified install script

--- extra/install.sh
+++ extra/install.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 # Check if nodejs is installed
-if which node > /dev/null
+if ! type node > /dev/null
 then
     echo "Please install node.js and re-run this script"
     exit 1
 fi
 
 # Check if MongoDB is installed
-if which mongod > /dev/null
+if ! type mongod > /dev/null
 then
     echo "Please install MongoDB and re-run this script"
     exit 1
 fi
 
 # Check if git is installed
-if which git > /dev/null
+if ! type git > /dev/null
 then
     echo "Please install git and re-run this script"
     exit 1
@@ -25,4 +25,4 @@ cd ~
 git clone https://github.com/littlejavachild/rest_api.git
 cd rest_api
 npm install
-npm start &
+npm start
