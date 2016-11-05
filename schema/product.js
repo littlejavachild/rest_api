e241291a1e3acedc776fb185bb4728aea1e76096
Fasih Khatib <fasih.khatib@raweng.com>
2016-11-05 14:07:50 +0530

    adding tests

--- schema/product.js
+++ schema/product.js
@@ -1,7 +1,8 @@
 module.exports = {
-    "title" : { "type" : String, required : true },
-    "category" : { "type" : String, required : true },
-    "price" : { "type" : Number, required : true },
-    "quantity" : { "type" : Number, required : true },
+    "_id" : { "type" : String },
+    "title" : { "type" : String, required : true, minlength : 3 },
+    "category" : { "type" : String, required : true, minlength : 3 },
+    "price" : { "type" : Number, required : true, min : 0, max : 100000 },
+    "quantity" : { "type" : Number, required : true, min : 0, max : 100000 },
     "status" : { "type" : Number, min : 0, max : 5, required : true }
 };
